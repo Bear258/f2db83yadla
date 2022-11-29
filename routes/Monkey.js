a04dd34c4f6a42a1313bff112f2c6bbe94b46df5
Bear258 <s556383@nwmissouri.edu>
2022-11-29 14:01:24 -0600

    second

--- routes/Monkey.js
+++ routes/Monkey.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on 
+//or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 
 const Monkey_controllers= require('../controllers/Monkey');
 
@@ -7,7 +17,7 @@ const Monkey_controllers= require('../controllers/Monkey');
 router.get('/',Monkey_controllers.Monkey_view_all_Page );
 router.get('/detail', Monkey_controllers.Monkey_view_one_Page);
 router.get('/create', Monkey_controllers.Monkey_create_Page);
-router.get('/update', Monkey_controllers.Monkey_update_Page);
+router.get('/update',secured, Monkey_controllers.Monkey_update_Page);
 router.get('/delete', Monkey_controllers.Monkey_delete_Page);
 
 module.exports = router;
